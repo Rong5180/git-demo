34dffe4c2f15d77b3918884e97a8686d81739432
wangrong <wangrong.rong5180@bytedance.com>
2021-08-04 17:50:42 +0800

    wu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Item from "./Item";
 
 function App() {
+  // input 数值
+  const [inputValue, setInputValue] = useState('')
+  const [list, setList] = useState([{ id: 0, con: 'dsas' }])
+  console.log(list);
+
+  const addItem = () => {
+    let item = {}
+    item.id = list.length + 1
+    item.con = inputValue
+    setList(() => [...list, item])
+  }
+
+  const deleteClick = (id) => {
+    setList((list) => {
+      return list.filter(v => v.id !== id)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input value={inputValue} onChange={e => setInputValue(e.target.value)} ></input><button onClick={addItem}>添加</button>
+      <Item list={list} deleteClick={deleteClick}></Item>
+    </>
   );
 }
 
